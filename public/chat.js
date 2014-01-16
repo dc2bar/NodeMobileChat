eb97759284628c09917d6c0615700c4e8c077e23
dc2bar <dc2bar@gmail.com>
2014-01-15 21:05:23 -0700

    added jQuery from google CDN

--- public/chat.js
+++ public/chat.js
@@ -1,23 +1,25 @@
-var messages = [];
-var socket = io.connect('/');
-var field = document.getElementById("field");
-var sendButton = document.getElementById("send");
-var content = document.getElementById("content");
+$(function(){
+    var messages = [];
+    var socket = io.connect('/');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
 
-socket.on('message', function (data) {
-    if (data.message) {
-        messages.push(data.message);
-        var html = '';
-        for (var i = 0; i < messages.length; i++) {
-            html += messages[i] + '<br />';
+    socket.on('message', function (data) {
+        if (data.message) {
+            messages.push(data.message);
+            var html = '';
+            for (var i = 0; i < messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
         }
-        content.innerHTML = html;
-    } else {
-        console.log("There is a problem:", data);
-    }
-});
+    });
 
-sendButton.onclick = function () {
-    var text = field.value;
-    socket.emit('send', { message: text });
-};
+    sendButton.onclick = function () {
+        var text = field.value;
+        socket.emit('send', { message: text });
+    };
+});
