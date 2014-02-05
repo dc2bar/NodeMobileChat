9b4ce034eeb2dab7c48f084d529d5a78c2829611
dc2bar <dc2bar@gmail.com>
2014-02-05 16:01:36 -0700

    debug

--- models/user_model.js
+++ models/user_model.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 exports.userSchema = new mongoose.Schema({
   socket_id: '',
   nickname: '',
-  font: '',
-  level: ''
+  font: '#0000FF|b',
+  level: 'user'
 });
 
 exports.userModel = mongoose.model('users', exports.userSchema);
